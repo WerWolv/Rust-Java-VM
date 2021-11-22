d7edbe0380b50172687e01361c840fd2ac0a0368
WerWolv <werwolv98@gmail.com>
2021-11-22 23:42:34 +0100

    vm: Started implementing opcodes

--- src/java/vm.rs
+++ src/java/vm.rs
@@ -1,6 +1,7 @@
 #![allow(dead_code)]
 
 use crate::java;
+use crate::java::class::ConstantPoolEntry;
 use crate::java::opcodes::Opcode;
 
 #[derive(Clone)]
@@ -21,26 +22,66 @@ pub struct Scope {
 }
 
 pub struct VirtualMachine {
-    pub jar: java::Jar,
+    pub main_jar: java::Jar,
+    pub library_jars: Vec<java::Jar>,
+
+    pub curr_class_name: String
 }
 
 impl VirtualMachine {
 
-    pub fn new(jar: java::Jar) -> Self {
-        VirtualMachine {
-            jar
+    pub fn new(jar: java::Jar) -> Option<Self> {
+        if let Some((name, class)) = jar.get_main_class() {
+            return Some(VirtualMachine {
+                main_jar: jar,
+                library_jars: vec![],
+                curr_class_name: name
+            });
+        } else {
+            println!("Cannot find main class!");
         }
+
+        None
+    }
+
+    pub fn add_library_jar(&mut self, jar: java::Jar) {
+        self.library_jars.push(jar);
+    }
+
+    fn get_constant_pool_entry(&self, index: usize) -> Option<&ConstantPoolEntry> {
+        Some(&self.get_current_class().class_file.constant_pool[(index - 1) as usize])
+    }
+
+    fn get_current_class(&self) -> &java::Class {
+        self.main_jar.classes.get(&*self.curr_class_name).unwrap()
     }
 
     fn execute_byte_code(&self, byte_code: &Vec<u8>, scope: &mut Scope) {
-        let mut pc = 0;
-        while pc < byte_code.len() {
-            let value = byte_code.get(pc).unwrap();
-            let opcode : Opcode = unsafe { std::mem::transmute(*value) };
+        let mut pc: usize = 0;
 
-            println!("    0x{:02X} {} ", value, opcode);
+        let option = |index: usize| -> u32 { *byte_code.get(index).unwrap() as u32 };
+
+        while pc < byte_code.len() {
+            let value = option(pc) as u8;
+            let opcode : Opcode = unsafe { std::mem::transmute(value) };
+
+            println!("    0x{:02X} {}", value, opcode);
 
             match opcode {
+                Opcode::getstatic => {
+                    let index = option(pc + 1) << 8 | option(pc + 2);
+                    if let Some(ConstantPoolEntry::FieldReference(class_index, name_and_type_index)) = self.get_constant_pool_entry((index) as usize) {
+                        if let Some(ConstantPoolEntry::ClassReference(name_index)) = self.get_constant_pool_entry(*class_index as usize) {
+                            println!("{}", self.get_current_class().class_file.get_constant_pool_string(*name_index as usize).unwrap());
+                        }
+                        if let Some(ConstantPoolEntry::NameAndTypeDescriptor(name_index, type_index)) = self.get_constant_pool_entry(*name_and_type_index as usize) {
+                            println!("{}", self.get_current_class().class_file.get_constant_pool_string(*name_index as usize).unwrap());
+                            println!("{}", self.get_current_class().class_file.get_constant_pool_string(*type_index as usize).unwrap());
+                        }
+                    } else {
+                        println!("Invalid constant pool entry!");
+                    }
+                },
                 Opcode::nop => { },
                 _ => {}//panic!("Invalid opcode!")
             }
@@ -75,14 +116,10 @@ impl VirtualMachine {
     }
 
     pub fn run(&self) {
-        if let Some(main_class) = self.jar.get_main_class() {
-            if let Some(main_method) = main_class.methods.get("main") {
-                self.execute_method(main_method);
-            } else {
-                println!("Cannot find main method!");
-            }
+        if let Some(main_method) = self.main_jar.classes.get(&*self.curr_class_name).unwrap().methods.get("main") {
+            self.execute_method(main_method);
         } else {
-            println!("Cannot find main class!");
+            println!("Cannot find main method!");
         }
     }
 
